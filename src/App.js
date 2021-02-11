d4d6b8f8696baa18f00cfa404d9bc96bd7b80b3f
Gabriel Ferreira <57750912+gabrielferreira2626@users.noreply.github.com>
2021-02-11 03:11:02 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { StateMachineProvider, createStore, useStateMachine } from "little-state-machine";
+import { useForm, useFieldArray, useWatch } from "react-hook-form";
+
+createStore({
+  yourDetails: {
+    firstName: "",
+    lastName: "",
+    categories: [
+      {
+        categoryName: "",
+        categoryDescription: "",
+      },
+    ],
+  },
+});
+
+function updateAction(state, payload) {
+  return {
+    ...state,
+    yourDetails: {
+      ...state.yourDetails,
+      ...payload,
+    },
+  };
+}
+
+const Page = () => {
+  const { state, actions } = useStateMachine({ updateAction });
+  const { handleSubmit, register, control } = useForm({
+    defaultValues: state.yourDetails,
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "categories",
+  });
+
+  //Save Data
+  const onSubmit = (data) => {
+    actions.updateAction(data);
+    console.log(state);
+  };
+
+  return (
+    <React.Fragment>
+      <h1>Dynamic Form with React Hook Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input ref={register} name="firstName" placeholder="First Name"></input>
+        <input ref={register} name="lastName" placeholder="LastName"></input>
+        {fields.map((index) => {
+          return (
+            <div>
+              <input ref={register} name={`categories[${index}].categoryName`} placeholder="LastName"></input>
+              <input ref={register} name={`categories[${index}].categoryDescription`} placeholder="LastName"></input>
+            </div>
+          );
+        })}
+
+        <button type="submit">Guardar</button>
+      </form>
+    </React.Fragment>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StateMachineProvider>
+      <Page />
+    </StateMachineProvider>
   );
 }
 
